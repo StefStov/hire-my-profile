e9f23c403388b45691ec9661946849362d6c7320
Stefanie <stefanie.stov@hotmail.com>
2020-10-14 09:50:34 +0200

    Styling and content changes

--- src/components/Languages/Languages.vue
+++ src/components/Languages/Languages.vue
@@ -4,54 +4,11 @@
 	</router-link>
 	<div class="languagesPage">
 		<div class="blocks">
-			<h3>PHP</h3>
+			<h3>Languages</h3>
 			<ul>
-				<li>WordPress</li>
-				<li>Drupal 7</li>
-				<li>Magento</li>
-				<li>Created my own CMS in PHP</li>
-			</ul>
-		</div>
-		<div class="blocks">
-			<h3>HTML</h3>
-			<ul>
-				<li>WordPress</li>
-				<li>Drupal 7</li>
-				<li>React</li>
-				<li>Vue</li>
-			</ul>
-		</div>
-		<div class="blocks">
-			<h3>CSS/CSS3</h3>
-			<ul>
-				<li>WordPress</li>
-				<li>Drupal 7</li>
-				<li>React</li>
-				<li>Vue</li>
-			</ul>
-		</div>
-		<div class="blocks">
-			<h3>Javascript</h3>
-			<ul>
-				<li>React</li>
-				<li>Vue</li>
-			</ul>
-		</div>
-		<div class="blocks">
-			<h3>Jquery</h3>
-			<ul>
-				<li>WordPress</li>
-				<li>Drupal 7</li>
-				<li>Bootstrap</li>
-			</ul>
-		</div>
-		<div class="blocks">
-			<h3>SCSS</h3>
-			<ul>
-				<li>WordPress</li>
-				<li>Drupal 7</li>
-				<li>Magento</li>
-				<li>Created my own CMS in PHP</li>
+				<li>English</li>
+				<li>German</li>
+				<li>Dutch</li>
 			</ul>
 		</div>
 	</div>
